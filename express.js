dcff7f326257f84cd8ccb41475b71c0a52173528
shulav karki <shulavkarki88@gmail.com>
2018-12-30 22:30:23 +0545

    add info and css fille

--- express.js
+++ express.js
@@ -13,27 +13,27 @@ app.set('view engine', 'hbs');
 // });
 
 
-hbs.registerPartials(__dirname + '/views/partials/');
-hbs.registerHelper('currentDateYear',() => {
-  return new Date().getFullYear();
-})
-hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+// hbs.registerPartials(__dirname + '/views/partials/');
+// hbs.registerHelper('currentDateYear',() => {
+//   return new Date().getFullYear();
+// })
+// hbs.registerHelper('screamIt', (text) => text.toUpperCase());
 
 
 
 app.use(express.static(__dirname + '/public'));
 app.get('/',(req,res) => {
   res.render('home.hbs',{
-    titlePage:"ShulavkarkiTitle",
+    // titlePage:"ShulavkarkiTitle",
 
   });
 });
-app.get('/about',(req,res) => {
-  res.render('about.hbs',{
-    titlePage:"ShulavkarkiTitle",
-
-  });
-});
+// app.get('/about',(req,res) => {
+//   res.render('about.hbs',{
+//     titlePage:"ShulavkarkiTitle",
+//
+//   });
+// });
 app.listen(port,() => {
   console.log(`Port ${port} is up and running.`);
 
