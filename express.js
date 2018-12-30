9de8573e9f8b432310e0d6db29dbb43461ff7417
shulav karki <shulavkarki88@gmail.com>
2018-12-30 22:04:27 +0545

    modified package.json and heroku pot

--- express.js
+++ express.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
+var port = process.env.PORT || 3000;
 var hbs = require('hbs');
-app.use((req,res,next) => {
-  console.log(req.method);
-  console.log(req.url);
-  next();
-});
+app.set('view engine', 'hbs');
+// app.use((req,res,next) => {
+//   console.log(req.method);
+//   console.log(req.url);
+//   next();
+// });
 // app.use((req,res,next) => {
 //   res.render('maintenance.hbs');
 // });
-app.use(express.static(__dirname + '/public'));
+
 
 hbs.registerPartials(__dirname + '/views/partials/');
 hbs.registerHelper('currentDateYear',() => {
@@ -18,7 +20,8 @@ hbs.registerHelper('currentDateYear',() => {
 hbs.registerHelper('screamIt', (text) => text.toUpperCase());
 
 
-app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
 app.get('/',(req,res) => {
   res.render('home.hbs',{
     titlePage:"ShulavkarkiTitle",
@@ -31,7 +34,7 @@ app.get('/about',(req,res) => {
 
   });
 });
-app.listen(3000,() => {
-  console.log("Port 3000 is up and running.");
-  
+app.listen(port,() => {
+  console.log(`Port ${port} is up and running.`);
+
 });
